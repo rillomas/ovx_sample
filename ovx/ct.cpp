698af5cb3e33958123ab8998acbf19296c3b595f
Masato Hori <rillomas@gmail.com>
2019-04-11 11:03:47 +0900

    removed abseil and fixed indent

--- ovx/ct.cpp
+++ ovx/ct.cpp
@@ -15,27 +15,27 @@ static vx_status map_and_convert(
 	vx_map_id *map,
 	vx_imagepatch_addressing_t *addr,
 	vx_image img) {
-  vx_uint32 width = 0, height = 0, planes = 0;
-  CHECK_VX_STATUS(vxQueryImage(img, VX_IMAGE_WIDTH,  &width,  sizeof(width)));
-  CHECK_VX_STATUS(vxQueryImage(img, VX_IMAGE_HEIGHT, &height, sizeof(height)));
-  CHECK_VX_STATUS(vxQueryImage(img, VX_IMAGE_PLANES, &planes, sizeof(planes)));
+	vx_uint32 width = 0, height = 0, planes = 0;
+	CHECK_VX_STATUS(vxQueryImage(img, VX_IMAGE_WIDTH,  &width,  sizeof(width)));
+	CHECK_VX_STATUS(vxQueryImage(img, VX_IMAGE_HEIGHT, &height, sizeof(height)));
+	CHECK_VX_STATUS(vxQueryImage(img, VX_IMAGE_PLANES, &planes, sizeof(planes)));
 
-  vx_rectangle_t rect = { 0, 0, width, height };
-  void* ptr = nullptr;
-  CHECK_VX_STATUS(vxMapImagePatch(img,  &rect, 0, map, addr,  &ptr, VX_READ_ONLY, VX_MEMORY_TYPE_HOST, VX_NOGAP_X));
-  converted->data = (uint8_t*)ptr;
-  converted->width = width;
-  converted->height = height;
-  converted->channels = planes;
+	vx_rectangle_t rect = { 0, 0, width, height };
+	void* ptr = nullptr;
+	CHECK_VX_STATUS(vxMapImagePatch(img,  &rect, 0, map, addr,  &ptr, VX_READ_ONLY, VX_MEMORY_TYPE_HOST, VX_NOGAP_X));
+	converted->data = (uint8_t*)ptr;
+	converted->width = width;
+	converted->height = height;
+	converted->channels = planes;
 	return VX_SUCCESS;
 }
 
 vx_status VX_CALLBACK back_projection_host(
-  vx_node node,
-  const vx_reference* refs,
-  vx_uint32 num) {
+	vx_node node,
+	const vx_reference* refs,
+	vx_uint32 num) {
 	if (num != BACK_PROJECTION_PARAM_NUM) {
-	  return VX_ERROR_INVALID_PARAMETERS;
+		return VX_ERROR_INVALID_PARAMETERS;
 	}
 
 	auto input = (vx_image)refs[0];
@@ -61,37 +61,41 @@ vx_status VX_CALLBACK back_projection_host(
 }
 
 vx_status back_projection_validator(
-  vx_node node,
-  const vx_reference parameters[],
-  vx_uint32 num,
-  vx_meta_format metas[]) {
+	vx_node node,
+	const vx_reference parameters[],
+	vx_uint32 num,
+	vx_meta_format metas[]) {
 	return VX_SUCCESS;
 }
 
 
 vx_status register_user_kernel(vx_context ctx) {
-  vx_kernel kernel = vxAddUserKernel(ctx,
-    BACK_PROJECTION_NAME.c_str(),
-    KernelID::BACK_PROJECTION,
-    back_projection_host,
-    BACK_PROJECTION_PARAM_NUM,
-    back_projection_validator,
-    nullptr,
-    nullptr);
-  CHECK_VX_OBJECT(kernel);
-  auto res = VX_SUCCESS;
-  vxAddLogEntry((vx_reference)ctx,
-    res,
-    "OK: registered user kernel %s\n",
-    BACK_PROJECTION_NAME.c_str());
-  return res;
+	vx_kernel kernel = vxAddUserKernel(ctx,
+		BACK_PROJECTION_NAME.c_str(),
+		KernelID::BACK_PROJECTION,
+		back_projection_host,
+		BACK_PROJECTION_PARAM_NUM,
+		back_projection_validator,
+		nullptr,
+		nullptr);
+	CHECK_VX_OBJECT(kernel);
+	CHECK_VX_STATUS(vxAddParameterToKernel(kernel, 0, VX_INPUT,  VX_TYPE_IMAGE,  VX_PARAMETER_STATE_REQUIRED));
+	CHECK_VX_STATUS(vxAddParameterToKernel(kernel, 1, VX_OUTPUT, VX_TYPE_IMAGE,  VX_PARAMETER_STATE_REQUIRED));
+	CHECK_VX_STATUS(vxFinalizeKernel(kernel));
+	CHECK_VX_STATUS(vxReleaseKernel(&kernel));
+	auto res = VX_SUCCESS;
+	vxAddLogEntry((vx_reference)ctx,
+		res,
+		"OK: registered user kernel %s\n",
+		BACK_PROJECTION_NAME.c_str());
+	return res;
 
 }
 
 vx_node back_projection_node(
-  vx_graph graph,
-  vx_image input,
-  vx_image output) {
+	vx_graph graph,
+	vx_image input,
+	vx_image output) {
 }
 
 } // namespace ct
